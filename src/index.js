b7f76fca62ba6f732cf8666bb393f354f86aef27
Marcio Galli <taboca@gmail.com>
2018-10-10 19:00:38 -0300

    Importing redux and react-redux

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* Redux setup */
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+ReactDOM.render((
+  <App />
+), document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
